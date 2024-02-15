23d28827fbedf193f675c8f4d8426aeb6355f697
danielBravo13 <154629767+danielBravo13@users.noreply.github.com>
2024-02-14 19:55:02 -0800

    Orange Ghost Movement

--- pacMan.cpp
+++ pacMan.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <allegro.h>
+#include <time.h>
 
+#define VELOCIDAD      100 //No usar valores negativos 
 void init();
 void motorJuego();
 void cargarMapa1(int matrizJuego[20][30]);
 void pintarMapa(int matrizJuego[20][30], BITMAP *buffer);
 void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]);
+void fantasmaNaranja(int matrizJuego[20][30], int orangeGhost[2]);
+void fantasmaSalir(int matrizJuego[20][30], int orangeGhost[2], int gNum);
 
 
 int main(){
+	srand(time(NULL));
 	init();
 	motorJuego();
 	return 0;
@@ -39,19 +44,31 @@ void init(){
 void motorJuego(){
 	BITMAP *buffer = create_bitmap(960,660);
     int op=2;
+    int contRep=0;
     int matrizJuego[20][30];
     int pacManPos[2];
+    int orangeGhostPos[2];
+    orangeGhostPos[0]=10;
+    orangeGhostPos[1]=15;
     pacManPos[0]=14;
     pacManPos[1]=14;
     cargarMapa1(matrizJuego);
     do{
     	pintarMapa(matrizJuego, buffer);
-        //movimientoPacMan(matrizJuego,pacManPos);
+        movimientoPacMan(matrizJuego,pacManPos);
         blit(buffer, screen, 0,0,0,0,960,660);
         clear(buffer);
+        rest(VELOCIDAD);
+        contRep++;
+        if(contRep==100){
+        	fantasmaSalir(matrizJuego,orangeGhostPos,9);
+        	
+		}
+		if(contRep>=120){
+			fantasmaNaranja(matrizJuego,orangeGhostPos);
+		}
     }while(op==2);
     
-
 }
 
 void cargarMapa1(int matrizJuego[20][30]){
@@ -94,6 +111,10 @@ void pintarMapa(int matrizJuego[20][30], BITMAP *buffer){
 	vectorMapa[2]= load_bitmap("PuntosChicos.bmp", NULL);
 	vectorMapa[3]= load_bitmap("PuntosGrandes.bmp", NULL);
 	vectorMapa[4]= load_bitmap("Espacio.bmp", NULL);
+	vectorMapa[6]= load_bitmap("FantasmaBlinky_Abajo.bmp", NULL);
+	vectorMapa[8]= load_bitmap("FantasmaInky_Abajo.bmp", NULL);
+	vectorMapa[9]= load_bitmap("FantasmaClyde_Abajo.bmp", NULL);
+	vectorMapa[7]= load_bitmap("FantasmaPinky_Abajo.bmp", NULL);
     for(int i=0;i<20;i++){
         for(int j=0;j<30;j++){
         	if(matrizJuego[i][j]==0){
@@ -111,14 +132,24 @@ void pintarMapa(int matrizJuego[20][30], BITMAP *buffer){
 			else if(matrizJuego[i][j]==2){
 				draw_sprite(buffer, vectorMapa[4], j*30, i*30+35);
 			}
-			
+			else if(matrizJuego[i][j]==6){
+				draw_sprite(buffer, vectorMapa[6], j*30, i*30+35);
+			}
+			else if(matrizJuego[i][j]==8){
+				draw_sprite(buffer, vectorMapa[8], j*30, i*30+35);
+			}
+			else if(matrizJuego[i][j]==9){
+				draw_sprite(buffer, vectorMapa[9], j*30, i*30+35);
+			}
+			else if(matrizJuego[i][j]==7){
+				draw_sprite(buffer, vectorMapa[7], j*30, i*30+35);
+			}
         }  
     }
 }
 
 void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]){
-    
-    
+    //UP
     if(key[KEY_W]){
         if(matrizJuego[posPacMan[0]-1][posPacMan[1]]!=1){
             matrizJuego[posPacMan[0]][posPacMan[1]]=2;
@@ -126,6 +157,7 @@ void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]){
             matrizJuego[posPacMan[0]][posPacMan[1]]=0;
         }   
     }
+    //DOWN
     else if(key[KEY_S]){
         if(matrizJuego[posPacMan[0]+1][posPacMan[1]]!=1){
             matrizJuego[posPacMan[0]][posPacMan[1]]=2;
@@ -134,7 +166,8 @@ void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]){
         }
         
     }
-    else if(key[KEY_D]){
+    //RIGHT
+    else if(key[KEY_D]){//y                  //x
         if(posPacMan[0]==10 && posPacMan[1]==29){
             matrizJuego[posPacMan[0]][posPacMan[1]]=2;
             posPacMan[0]=10;
@@ -146,8 +179,9 @@ void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]){
             matrizJuego[posPacMan[0]][posPacMan[1]]=2;
             posPacMan[1]=posPacMan[1]+1;
             matrizJuego[posPacMan[0]][posPacMan[1]]=0;
-        } 
-    }
+        }
+	}
+    //LEFT
     else if(key[KEY_A]){
         if(posPacMan[0]==10 && posPacMan[1]==0){
             matrizJuego[posPacMan[0]][posPacMan[1]]=2;
@@ -162,4 +196,56 @@ void movimientoPacMan(int matrizJuego[20][30], int posPacMan[2]){
         }
     }   
 }
+void fantasmaNaranja(int matrizJuego[20][30], int orangeGhost[2]){
+	int movement;
+	
+	
+//	movement= rand()%4;
+//	//UP
+//	if(movement==0){
+//		if(matrizJuego[orangeGhost[0]-1][orangeGhost[1]]!=1){
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=2; //replace
+//			orangeGhost[0]=orangeGhost[0]-1;
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=9;			
+//		}		
+//	}
+//	//DOWN
+//	if(movement==1){
+//		if(matrizJuego[orangeGhost[0]+1][orangeGhost[1]]!=1){
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=2; //replace
+//			orangeGhost[0]=orangeGhost[0]+1;
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=9;			
+//		}		
+//	}
+//	//RIGHT
+//	if(movement==2){
+//		if(matrizJuego[orangeGhost[0]][orangeGhost[1]+1]!=1){
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=2; //replace
+//			orangeGhost[1]=orangeGhost[1]+1;
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=9;			
+//		}		
+//	}
+//	//LEFT
+//	if(movement==3){
+//		if(matrizJuego[orangeGhost[0]][orangeGhost[1]-1]!=1){
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=2; //replace
+//			orangeGhost[1]=orangeGhost[1]-1;
+//			matrizJuego[orangeGhost[0]][orangeGhost[1]]=9;			
+//		}		
+//	}
+	
+}
+	
+void fantasmaSalir(int matrizJuego[20][30], int orangeGhost[2], int gNum){
+	if(gNum==9){
+		matrizJuego[orangeGhost[0]][orangeGhost[1]]=2;
+		orangeGhost[0]=8;
+		orangeGhost[1]=12;
+		matrizJuego[orangeGhost[0]][orangeGhost[1]]=9;
+	}
+	
+}	
+	
+	
+
 
